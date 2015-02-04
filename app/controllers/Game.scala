14e5fcbd180b2396ab417eaf8e67c0fb653b3646
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-03 20:18:27 -0500

    :boom::boom::boom:
    Can connect to a websocket. Doesn't really do anything.

--- app/controllers/Game.scala
+++ app/controllers/Game.scala
@@ -1,9 +1,27 @@
 package controllers
 
+import scala.concurrent.duration.DurationInt
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.iteratee._
+import play.api.libs.concurrent.Promise
 import play.api.mvc._
 
 object Game extends Controller {
-  def index = Action {
-    Ok(views.html.game.index())
+  def index = Action { implicit request =>
+    Ok(views.html.game.index(request))
+  }
+
+  def gameSocket = WebSocket.using[String] { implicit request =>
+    println("connected")
+    val in = Iteratee.foreach[String] { chunk =>
+      println(chunk)
+    }
+
+    val out = Enumerator.repeatM {
+      Promise.timeout("hi", 3.seconds)
+    }
+
+    (in, out)
   }
 }
--- public/javascripts/space_wars.js
+++ public/javascripts/space_wars.js
@@ -1,91 +1,109 @@
-var SPACE_KEY = 32;
-var A_KEY = 65;
-var Q_KEY = 81;
-var P_KEY = 80;
-var L_KEY = 76;
-var LEFT_KEY = 37;
-var UP_KEY = 38;
-var RIGHT_KEY = 39;
-var DOWN_KEY = 40;
+$(document).ready(function() {
+  var SPACE_KEY = 32;
+  var A_KEY = 65;
+  var Q_KEY = 81;
+  var P_KEY = 80;
+  var L_KEY = 76;
+  var LEFT_KEY = 37;
+  var UP_KEY = 38;
+  var RIGHT_KEY = 39;
+  var DOWN_KEY = 40;
 
-function drawRect(x, y, w, h, color) {
-  ctx.strokeStyle = color;
-  ctx.fillStyle = color;
+  function drawRect(x, y, w, h, color) {
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 
-  ctx.fillRect(x, y, w, h);
-}
-
-function drawCircle(x, y, radius, color) {
-  ctx.strokeStyle = color;
-  ctx.fillStyle = color;
-
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-  ctx.fill();
-}
-
-var canvas = document.getElementById("game-canvas");
-var ctx = canvas.getContext("2d");
-
-var SCREEN_WIDTH = ctx.canvas.width;
-var SCREEN_HEIGHT = ctx.canvas.height;
-
-var position = { x: SCREEN_WIDTH / 2, y: SCREEN_HEIGHT / 2 };
-
-function draw() {
-  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-  drawCircle(position.x, position.y, 30, "blue");
-}
-
-function tick() {
-}
-
-function loop(time) {
-  tick();
-  draw();
-
-  window.requestAnimationFrame(function(time) {
-    loop(time);
-  });
-}
-
-function keyDown(event) {
-  var handled = true;
-
-  switch (event.keyCode) {
-
-  case UP_KEY:
-    position.y -= 10;
-    break;
-
-  case DOWN_KEY:
-    position.y += 10;
-    break;
-
-  case LEFT_KEY:
-    position.x -= 10;
-    break;
-
-  case RIGHT_KEY:
-    position.x += 10;
-    break;
-
-  default:
-    handled = false;
-    break;
+    ctx.fillRect(x, y, w, h);
   }
 
-  if (handled) {
-    event.preventDefault();
+  function drawCircle(x, y, radius, color) {
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.fill();
   }
-}
 
-function run() {
-  window.onkeydown = keyDown;
+  ws = new WebSocket($("#websocket-url").data("url"));
 
-  window.requestAnimationFrame(function(time) {
-    loop(time);
-  });
-}
+  var canvas = document.getElementById("game-canvas");
+  var ctx = canvas.getContext("2d");
 
-run();
+  var SCREEN_WIDTH = ctx.canvas.width;
+  var SCREEN_HEIGHT = ctx.canvas.height;
+
+  var position = { x: SCREEN_WIDTH / 2, y: SCREEN_HEIGHT / 2 };
+
+  function draw() {
+    ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    drawCircle(position.x, position.y, 30, "blue");
+  }
+
+  function tick() {
+  }
+
+  function loop(time) {
+    tick();
+    draw();
+
+    window.requestAnimationFrame(function(time) {
+      loop(time);
+    });
+  }
+
+  function keyDown(event) {
+    var handled = true;
+
+    switch (event.keyCode) {
+
+    case UP_KEY:
+      position.y -= 10;
+      break;
+
+    case DOWN_KEY:
+      position.y += 10;
+      break;
+
+    case LEFT_KEY:
+      position.x -= 10;
+      break;
+
+    case RIGHT_KEY:
+      position.x += 10;
+      break;
+
+    default:
+      handled = false;
+      break;
+    }
+
+    if (handled) {
+      ws.send(position);
+      event.preventDefault();
+    }
+  }
+
+  function run() {
+    window.onkeydown = keyDown;
+
+    window.requestAnimationFrame(function(time) {
+      loop(time);
+    });
+  }
+
+  run();
+
+
+  // ws.onmessage = function(msg) {
+  //   $("<li>").text(msg.data).appendTo("#messages");
+  // }
+
+  // $("#chatform").submit(function() {
+  //   ws.send($("#text").val());
+  //   $("#text").val("").focus();
+  //   return false;
+  // });
+  // });
+
+});
