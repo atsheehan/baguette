0873f8b9737e902afdaa2adda9ec04f310d22682
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-06 19:12:31 -0500

    Sync positions of multiple players.

--- app/controllers/Game.scala
+++ app/controllers/Game.scala
@@ -1,26 +1,75 @@
 package controllers
 
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.ExecutionContext.Implicits.global
 
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.iteratee._
+import akka.actor.{ Actor, Props, actorRef2Scala }
+import akka.actor.Actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import play.api.Play.current
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.libs.concurrent.Akka
 import play.api.libs.concurrent.Promise
-import play.api.mvc._
+import play.api.libs.iteratee.{ Concurrent, Enumerator, Iteratee }
+import play.api.mvc.{ Action, Controller, WebSocket }
+import scala.language.postfixOps
 
 object Game extends Controller {
-  def index = Action { implicit request =>
+  implicit val timeout = Timeout(1 seconds)
+  val world = Akka.system.actorOf(Props[World])
+  Akka.system.scheduler.schedule(0 seconds, 1 seconds, world, Broadcast())
+
+  def index = Action { request =>
     Ok(views.html.game.index(request))
   }
 
-  def gameSocket = WebSocket.using[String] { request =>
-    val in = Iteratee.foreach[String] { chunk =>
-      println(chunk)
-    }
-
-    val out = Enumerator.repeatM {
-      Promise.timeout("""[{"x": 30, "y": 30, "user": "atsheehan"}]""", 3.seconds)
-    }
-
-    (in, out)
+  def gameSocket = WebSocket.async { request =>
+    println("bloop")
+    val channel = world ? Join()
+    channel.mapTo[(Iteratee[String, _], Enumerator[String])]
   }
+}
+
+case class Player(val id: Int, var x: Float, var y: Float)
+case class Join()
+case class Broadcast()
+
+class World extends Actor {
+  var playerIndex = 0
+  var players = List[Player]()
+  val (enumerator, channel) = Concurrent.broadcast[String]
+
+  def receive = {
+    case Join() => {
+      println("player joined")
+
+      val player = new Player(playerIndex, 0f, 0f)
+      players = player :: players
+      playerIndex += 1
+
+      val iteratee = Iteratee.foreach[String] { command =>
+        command match {
+          case "left" => player.x -= 1f
+          case "right" => player.x += 1f
+          case "up" => player.y -= 1f
+          case "down" => player.y += 1f
+        }
+      }
+
+      sender ! (iteratee, enumerator)
+    }
+
+    case Broadcast() => {
+      println("BROADCASTING")
+      channel.push(Json.toJson(players).toString)
+    }
+  }
+
+  implicit val playerWrites: Writes[Player] = (
+    (JsPath \ "id").write[Int] and
+      (JsPath \ "x").write[Float] and
+      (JsPath \ "y").write[Float]
+  )(unlift(Player.unapply))
 }
