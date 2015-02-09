8e2160a0e98fb8607df2f46783a40f58c04501f1
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-08 22:09:06 -0500

    Update server-side world.
    Mirror updates from the client-side world.

--- app/models/Ship.scala
+++ app/models/Ship.scala
@@ -1,21 +1,60 @@
 package models
 
+object Rotation extends Enumeration {
+  val Clockwise = Value("Clockwise")
+  val CounterClockwise = Value("CounterClockwise")
+}
+
 class Ship(val id: Int, x: Float, y: Float) {
   private var _pos = Vec2(x, y)
   private var _vel = Vec2.ZERO
   private var _accel = Vec2.ZERO
   private var _heading = Vec2(1f, 0f)
 
+  private var _engineOn = false
+  private var _rotating: Option[Rotation.Value] = None
+
+  private val Thrust = 0.1f
+  private val RotationSpeed = (Math.PI / 20f).asInstanceOf[Float]
+
   def pos = _pos
   def vel = _vel
   def accel = _accel
+  def heading = _heading
+  def engineOn = _engineOn
+  def rotating = _rotating
 
   def update() {
+    if (engineOn) {
+      _accel = heading.scale(Thrust)
+    } else {
+      _accel = Vec2.ZERO
+    }
+
+    rotating match {
+      case Rotation.Clockwise => _heading = heading.rotate(RotationSpeed)
+      case Rotation.CounterClockwise => _heading = heading.rotate(-RotationSpeed)
+    }
+
     _vel = vel.add(accel)
     _pos = pos.add(vel)
   }
 
   def accelerate() {
     _accel = Vec2(0.01f, 0f)
+  }
+
+  def startTurning(rotation: Rotation.Value) {
+    _rotating = Some(rotation)
+  }
+
+  def stopTurning(rotation: Rotation.Value) {
+    if (_rotating == rotation) {
+      _rotating = None
+    }
+  }
+
+  def toggleEngine(value: Boolean) {
+    _engineOn = value;
   }
 }
--- app/models/Vec2.scala
+++ app/models/Vec2.scala
@@ -1,7 +1,24 @@
 package models
 
 class Vec2(val x: Float, val y: Float) {
-  def add(that: Vec2) = Vec2(this.x + that.x, this.y + that.y)
+  def add(that: Vec2): Vec2 = Vec2(this.x + that.x, this.y + that.y)
+
+  def magnitude: Float = Math.sqrt(x * x + y * y).asInstanceOf[Float]
+
+  def normalize: Vec2 =
+    if (magnitude == 0f) Vec2.ZERO
+    else Vec2(x / magnitude, y / magnitude)
+
+  def scale(factor: Float): Vec2 = Vec2(x * factor, y * factor)
+
+  def lengthTo(length: Float): Vec2 = normalize.scale(length)
+
+  def rotate(radians: Float): Vec2 = {
+    val cos = Math.cos(radians).asInstanceOf[Float]
+    val sin = Math.sin(radians).asInstanceOf[Float]
+
+    Vec2(x * cos - y * sin, x * sin + y * cos)
+  }
 }
 
 object Vec2 {
