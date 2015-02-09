69e08e312a0ed4d9ffa324cc53e8c4df039082e8
Adam Sheehan <a.t.sheehan@gmail.com>
2015-02-09 18:03:47 -0500

    Remove jQuery from JS game code.
    It's not needed. The game page uses its own layout that avoids
    any extraneous elements so that it can use up the entire screen.

--- public/javascripts/space_wars.js
+++ public/javascripts/space_wars.js
@@ -1,137 +1,135 @@
-$(document).ready(function() {
-  var view = new View(document.getElementById("game-canvas"));
-  var player = new Ship(0, new Vec2(100, 100));
-  var ships = [player];
+var view = new View(document.getElementById("game-canvas"));
+var player = new Ship(0, new Vec2(100, 100));
+var ships = [player];
 
-  function draw() {
-    view.clearScreen();
+function draw() {
+  view.clearScreen();
 
+  for (var i = 0; i < ships.length; i++) {
+    view.drawTriangle(ships[i].pos, ships[i].heading, 30, "blue");
+  }
+}
+
+var framesPerSecond = 30;
+var millisecondsPerFrame = (1 / framesPerSecond) * 1000;
+var millisecondsLastTick = 0;
+var timeCounter = 0;
+
+function tick(milliseconds) {
+  var timeElapsed = milliseconds - millisecondsLastTick;
+  millisecondsLastTick = milliseconds;
+  timeCounter += timeElapsed;
+
+  while (timeCounter > millisecondsPerFrame) {
     for (var i = 0; i < ships.length; i++) {
-      view.drawTriangle(ships[i].pos, ships[i].heading, 30, "blue");
-    }
-  }
-
-  var framesPerSecond = 30;
-  var millisecondsPerFrame = (1 / framesPerSecond) * 1000;
-  var millisecondsLastTick = 0;
-  var timeCounter = 0;
-
-  function tick(milliseconds) {
-    var timeElapsed = milliseconds - millisecondsLastTick;
-    millisecondsLastTick = milliseconds;
-    timeCounter += timeElapsed;
-
-    while (timeCounter > millisecondsPerFrame) {
-      for (var i = 0; i < ships.length; i++) {
-        ships[i].update();
-      }
-
-      timeCounter -= millisecondsPerFrame;
-    }
-  }
-
-  function loop(time) {
-    tick(time);
-    draw();
-
-    window.requestAnimationFrame(function(time) {
-      loop(time);
-    });
-  }
-
-  function keyDown(event) {
-    var handled = true;
-
-    switch (event.keyCode) {
-
-    case UP_KEY:
-      player.toggleEngine(true);
-      ws.send("engineOn")
-      break;
-
-    case LEFT_KEY:
-      player.startTurning("counter-clockwise");
-      ws.send("startTurnCC")
-      break;
-
-    case RIGHT_KEY:
-      player.startTurning("clockwise");
-      ws.send("startTurnC")
-      break;
-
-    default:
-      handled = false;
-      break;
+      ships[i].update();
     }
 
-    if (handled) {
-      event.preventDefault();
-    }
+    timeCounter -= millisecondsPerFrame;
+  }
+}
+
+function loop(time) {
+  tick(time);
+  draw();
+
+  window.requestAnimationFrame(function(time) {
+    loop(time);
+  });
+}
+
+function keyDown(event) {
+  var handled = true;
+
+  switch (event.keyCode) {
+
+  case UP_KEY:
+    player.toggleEngine(true);
+    ws.send("engineOn")
+    break;
+
+  case LEFT_KEY:
+    player.startTurning("counter-clockwise");
+    ws.send("startTurnCC")
+    break;
+
+  case RIGHT_KEY:
+    player.startTurning("clockwise");
+    ws.send("startTurnC")
+    break;
+
+  default:
+    handled = false;
+    break;
   }
 
-  function keyUp(event) {
-    var handled = true;
+  if (handled) {
+    event.preventDefault();
+  }
+}
 
-    switch (event.keyCode) {
+function keyUp(event) {
+  var handled = true;
 
-    case UP_KEY:
-      player.toggleEngine(false);
-      ws.send("engineOff")
-      break;
+  switch (event.keyCode) {
 
-    case LEFT_KEY:
-      player.stopTurning("counter-clockwise");
-      ws.send("stopTurnCC")
-      break;
+  case UP_KEY:
+    player.toggleEngine(false);
+    ws.send("engineOff")
+    break;
 
-    case RIGHT_KEY:
-      player.stopTurning("clockwise");
-      ws.send("stopTurnC")
-      break;
+  case LEFT_KEY:
+    player.stopTurning("counter-clockwise");
+    ws.send("stopTurnCC")
+    break;
 
-    default:
-      handled = false;
-      break;
-    }
+  case RIGHT_KEY:
+    player.stopTurning("clockwise");
+    ws.send("stopTurnC")
+    break;
 
-    if (handled) {
-      event.preventDefault();
-    }
+  default:
+    handled = false;
+    break;
   }
 
-  function run() {
-    window.onkeydown = keyDown;
-    window.onkeyup = keyUp;
-
-    window.requestAnimationFrame(function(time) {
-      loop(time);
-    });
+  if (handled) {
+    event.preventDefault();
   }
+}
 
-  ws = new WebSocket($("#websocket-url").data("url"));
+function run() {
+  window.onkeydown = keyDown;
+  window.onkeyup = keyUp;
 
-  ws.onmessage = function(message) {
-    data = JSON.parse(message.data);
+  window.requestAnimationFrame(function(time) {
+    loop(time);
+  });
+}
 
-    for (var i = 0; i < data.ships.length; i++) {
-      var shipData = data.ships[i];
+ws = new WebSocket(document.getElementById("websocket-url").dataset.url);
 
-      for (var j = 0; j < ships.length; j++) {
-        if (shipData.id === ships[j].id) {
-          var ship = ships[j];
+ws.onmessage = function(message) {
+  data = JSON.parse(message.data);
 
-          ship.pos.x = shipData.pos.x;
-          ship.pos.y = shipData.pos.y;
-          ship.heading.x = shipData.heading.x;
-          ship.heading.y = shipData.heading.y;
-          ship.vel.x = shipData.vel.x;
-          ship.vel.y = shipData.vel.y;
-          ship.accel.x = shipData.accel.x;
-          ship.accel.y = shipData.accel.y;
-        }
+  for (var i = 0; i < data.ships.length; i++) {
+    var shipData = data.ships[i];
+
+    for (var j = 0; j < ships.length; j++) {
+      if (shipData.id === ships[j].id) {
+        var ship = ships[j];
+
+        ship.pos.x = shipData.pos.x;
+        ship.pos.y = shipData.pos.y;
+        ship.heading.x = shipData.heading.x;
+        ship.heading.y = shipData.heading.y;
+        ship.vel.x = shipData.vel.x;
+        ship.vel.y = shipData.vel.y;
+        ship.accel.x = shipData.accel.x;
+        ship.accel.y = shipData.accel.y;
       }
     }
   }
+}
 
-  run();
-});
+run();
